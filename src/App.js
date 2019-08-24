1b236e3021503152fd0699e6d82effc0c6e62910
Christina Douglas <chrissyidouglas@gmail.com>
2019-08-24 12:51:22 -0700

    add route switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Login from './Login'
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Switch>
+      <Route exact path="/" render={(props) => <Login {...props} logIn={this.logIn} />} />
+      </Switch>
     </div>
   );
 }
