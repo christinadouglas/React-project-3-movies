f2bbb4e7fa7e7c69be651f5a745e4e2a8ead866e
Christina Douglas <chrissyidouglas@gmail.com>
2019-08-24 12:54:22 -0700

    login working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './Login'
 
-function App() {
+const My404 = () =>{
+  return (
+    <div>
+      You are lost
+    </div>
+    )
+}
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      username: '',
+      email: '',
+      image: '',
+      loading: true,
+    }
+  }
+  logIn = async (loginInfo) => {
+    try {
+
+      const loginResponse = await fetch('http://localhost:8000/user/login', {
+        method: 'POST',
+        credentials: 'include',// on every request we have to send the cookie
+        body: JSON.stringify(loginInfo),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      const parsedResponse = await loginResponse.json();
+
+
+      this.setState(() => {
+        return {
+          ...parsedResponse.data,
+          loading: false
+        }
+      })
+
+      return parsedResponse
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  render() {
+
   return (
     <div className="App">
       <Switch>
@@ -11,6 +58,7 @@ function App() {
       </Switch>
     </div>
   );
+}
 }
 
 export default App;
--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Form, Grid, Header, Image, Message, Segment} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+
 class Login extends Component {
   constructor(){
     super();
